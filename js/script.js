48fed2f641ccd05fcf5cdd41a59e5f8d3f7a9d46
kelly_k <pinkflamingo681@gmail.com>
2024-11-09 12:01:48 -0500

    reveal a random image

--- js/script.js
+++ js/script.js
@@ -4,8 +4,24 @@ const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
 
 const getImage = async function () {
-    const res = await fetch ("https://picsum.photos/v2/list?limit=100");
-    cont images = await res.json();
-    console.log(images);
-}
-getImage();
+  const res = await fetch("https://picsum.photos/v2/list?limit=100");
+  const images = await res.json();
+  selectRandomImage(images);
+};
+
+const randomIndex = math.floor(math.random()*images.length);
+//console.log(randomIndex);
+const randomImage = images[randomIndex];
+//console.log(randomImage);
+displayImage(randomImage);
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+  };
+  
+  button.addEventListener("click", function () {
+    getImage();
+  });
